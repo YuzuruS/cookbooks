e01c9d26d6e2b457ca1090593f7bf0c7b6aff4ed
yudsuzuk <navitima@gmail.com>
2013-11-29 16:52:26 +0900

    add resolv.conf

--- other/recipes/default.rb
+++ other/recipes/default.rb
@@ -12,6 +12,12 @@
   end
 end
 
+template "resolv.conf" do
+    path "/etc/resolv.conf"
+    source "resolv.conf.erb"
+    mode 0644
+end
+
 #execute "oh-my-zsh-install" do
 #  command "git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh; cp /home/vagrant/.zshrc /home/vagrant/.zshrc.orig; cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc; chsh -s /bin/zsh"
 #end
