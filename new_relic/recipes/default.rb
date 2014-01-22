a6a8271b00a705af07241c72a58b6ff88eb32b49
yudsuzuk <navitima@gmail.com>
2014-01-22 12:55:55 +0900

    fix new_relic

--- new_relic/recipes/default.rb
+++ new_relic/recipes/default.rb
@@ -7,13 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 bash 'install_and_activate_newrelic' do
-  not_if 'rpm -qa ¦ grep newrelic-sysmond'
+  not_if 'rpm -qa | grep newrelic-sysmond'
   code <<-EOC
     sudo rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
     sudo yum install -y newrelic-sysmond newrelic-php5
-    sudo nrsysmond-conﬁg --set license_key=#{node[:newrelic_license_key]}
+    sudo nrsysmond-config --set license_key=#{node[:newrelic_license_key]}
     sudo /etc/init.d/newrelic-sysmond start
-    sudo /sbin/chkconﬁg newrelic-sysmond on
+    sudo /sbin/chkconfig newrelic-sysmond on
     sudo sed -ie "s/REPLACE_WITH_REAL_KEY/#{node[:newrelic_license_key]}/g" /etc/php.d/ newrelic.ini
     sudo /etc/init.d/httpd graceful
   EOC
