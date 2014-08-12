7b14909977e6e432a6809a201963f3558214da7a
YuzuruS <navitima@gmail.com>
2014-08-12 19:40:51 +0900

    support ntp

--- server_setting/recipes/default.rb
+++ server_setting/recipes/default.rb
@@ -22,3 +22,20 @@ service "iptables" do # <- 2
     supports :status => true, :restart => true
     action [:restart, :enable]
 end
+
+%w{ntp}.each do |p| # <- 1
+  package p do
+    action :install
+  end
+end
+
+template "ntp.conf" do
+    path "/etc/ntp.conf"
+    source "ntp.conf.erb"
+    mode 0644
+end
+
+service "ntp" do # <- 2
+    supports :restart=>true, :reload=>false, :status=>true
+    action [:restart, :enable]
+end
